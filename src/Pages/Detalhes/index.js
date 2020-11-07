4560e6189a1293c8b66715040442b33b6705ae4b
brunoGoncalves <digaprobruno@gmail.com>
2020-11-07 11:32:23 -0300

    Correção de Bug

--- src/Pages/Detalhes/index.js
+++ src/Pages/Detalhes/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Text } from 'react-native'
-import { Container, HeaderText } from './style'
 export default ({ route: { params: { data } } }) => {
 
     return (
-        <Container>
-            <HeaderText>{data.titulo}</HeaderText>
-        </Container>
+        
+        <Text>{data.titulo}</Text>
         
     )
 }
--- src/Pages/Detalhes/style.js
+++ src/Pages/Detalhes/style.js
@@ -1,13 +1,1 @@
 import styled from 'styled-components/native'
-
-export const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-`
-export const Header = styled.Text`
-    font-size: 16px;
-    font-weight: 700;
-    color: #a2a2a2;
-    flex-direction: row;
-
-`
