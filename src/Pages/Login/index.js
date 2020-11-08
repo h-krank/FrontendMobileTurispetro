9bfe994c034524455c616739ce70c9e19ecb9cc7
Raphael F. de Medeiros <68427627+mikhaya@users.noreply.github.com>
2020-11-08 01:02:01 -0300

    Add files via upload

---
+++ src/Pages/Login/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button,Image, ImageBackground, Text } from 'react-native'
+
+import {
+    BoxLogin,
+    FazerLogin,
+    Cadastrar,
+    Logo,
+    Titulo
+  } from "./styles";
+  
+  import FundoLogin from "../../assets/FundoLogin.png";
+  import BrasaoPetropolis from "../../assets/BrasaoPetropolis.png";
+
+export default Login => {
+
+    return (
+        <ImageBackground source={FundoLogin} style = {{resizeMode: "cover", flex: 1}} >
+            <Logo>
+            <Text>Prefeitura de Petropolis</Text>
+            </Logo>
+            <BoxLogin>
+                <Titulo> Seja bem-vindo a Petrópolis </Titulo>
+                <Button onPress={() => handlePress()} title="Fazer Login"/>
+                <Cadastrar onPress={() => navigation.navigate(Cadastrar)}>Cadastre-se</Cadastrar>
+            </BoxLogin>
+        </ImageBackground>
+    )
+}
---
+++ src/Pages/Login/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+
+export const Titulo = styled.Text`
+fontSize: 27px;
+color: #333333;
+fontWeight: bold;
+textAlign: center;
+`;
+
+export const BoxLogin = styled.View`
+borderTopLeftRadius: 30px;
+borderTopRightRadius: 30px;
+marginTop: auto;
+backgroundColor: #fff;
+paddingTop: 27px; 
+paddingLeft:84px; 
+paddingRight:84px;
+`;
+
+
+export const FazerLogin = styled.Text`
+textAlign: center;
+`;
+
+export const Cadastrar = styled.Text`
+textAlign: center;
+paddingTop: 0px; 
+paddingLeft:47px; 
+paddingRight:47px;
+margin-top: 17px
+margin-bottom: 17px
+`;
+
+export const Logo = styled.View`
+paddingLeft: 50%;
+`
